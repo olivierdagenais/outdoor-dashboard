9970d1a38d03923c470c565640c48d7217a517a5
Olivier Dagenais <olivier.dagenais@gmail.com>
2024-05-01 21:41:30 -0400

    refactor: move source root to src

--- observablehq.config.js
+++ observablehq.config.js
@@ -22,7 +22,7 @@ export default {
   footer: "", // what to show in the footer (HTML)
   // toc: true, // whether to show the table of contents
   // pager: true, // whether to show previous & next links in the footer
-  // root: "docs", // path to the source root for preview
+  root: "src", // path to the source root for preview
   // output: "dist", // path to the output root for build
   // search: true, // activate search
 };
