f828b5f0b1acde9e91ea2b10ac8870a820b0f657
Olivier Dagenais <olivier.dagenais@gmail.com>
2024-06-18 21:15:04 -0400

    feat: assume seconds are 00 if missing

--- src/modules/siteDataToGeoJSON.ts
+++ src/modules/siteDataToGeoJSON.ts
@@ -64,13 +64,14 @@ export function siteDataToGeoJSON(doc: Document): Feature {
 
 // 20240614010141 -> 2024-06-14T01:01:41+00:00
 export function utcTimeStampToIso8601(timeStamp: string): string {
+    const seconds = timeStamp.slice(12, 14);
     const result =
         timeStamp.slice(0, 4) +
         "-" + timeStamp.slice(4, 6) +
         "-" + timeStamp.slice(6, 8) +
         "T" + timeStamp.slice(8, 10) +
         ":" + timeStamp.slice(10, 12) +
-        ":" + timeStamp.slice(12, 14) +
+        ":" + (seconds ? seconds : "00") +
         "+00:00";
     return result;
 }
